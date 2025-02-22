769233b7eab235ca23ca443e3cc10f36566a67c4
EugeneWinter <corvoattano200529@gmail.com>
2025-02-23 02:49:25 +0300

    фильмы done

--- PuzzleLabyrinth/Utils/Room.cs
+++ PuzzleLabyrinth/Utils/Room.cs
@@ -7,14 +7,12 @@ namespace PuzzleLabyrinth.Utils
         public string Question { get; }
         public List<string> Options { get; }
         public string CorrectAnswer { get; }
-        public string Hint { get; }
 
         public Room(string question, string option1, string option2, string rightOption)
         {
             Question = question;
             Options = new List<string>() { option1, option2 };
             CorrectAnswer = rightOption;
-            Hint = $"Подсказка: правильный ответ - это {rightOption.Length} букв.";
         }
 
         public bool CheckAnswer(string choice)
